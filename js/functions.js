9a84fe17f30e1bc3cdf8cfaccdb43deafb24728e
xMAPKEPx <russia201554321@gmail.com>
2023-10-02 20:15:29 +0500

    Проблема в вызове консоли

--- js/functions.js
+++ js/functions.js
@@ -1,15 +1,15 @@
 const isFit = function (string, length) {
   return string.length<=length;
 };
+isFit('абвгдеж', 10);
 
 const IsPalindrome = function (string) {
   let reversedString = '';
   const correctString = string.replaceAll(' ', '').toLowerCase();
-  for (let i = correctString.length-1; i>=0; i--) {
-    reversedString+=correctString[i];
-  }
+  reversedString = [...correctString].reverse().join('');
   return correctString===reversedString;
 };
+IsPalindrome('Кекс');
 
 const getNumber = function (string) {
   string=string.toString();
@@ -21,9 +21,16 @@ const getNumber = function (string) {
       result+=number;
     }
   }
-  if (result==='') {
-    return 'NaN';
+  if (result===''){
+    return NaN;
   } else {
     return result;
   }
 };
+
+console.log(getNumber('2023 год'));            // 2023
+console.log(getNumber('ECMAScript 2022'));     // 2022
+console.log(getNumber('1 кефир, 0.5 батона')); // 105
+console.log(getNumber('агент 007'));           // 7
+console.log(getNumber('а я томат'));           // NaN
+
